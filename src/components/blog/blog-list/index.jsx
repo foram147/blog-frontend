985f191cbba17fb6fd220994e2c333da92db3979
foram patel <p.foram147@gmail.com>
2021-10-27 12:30:15 +0100

    connected to BE

--- src/components/blog/blog-list/index.jsx
+++ src/components/blog/blog-list/index.jsx
@@ -3,13 +3,31 @@ import { Row, Col } from "react-bootstrap";
 import BlogItem from "../blog-item";
 import posts from "../../../data/posts.json";
 export default class BlogList extends Component {
+  state = {
+    blogPosts: [],
+  };
+  async fetchBlogPosts() {
+    let response = await fetch(
+      `${process.env.REACT_APP_BE_PROD_URL}/blogPosts`
+    );
+    if (response.ok) {
+      let blogPosts = await response.json();
+      this.setState({ blogPosts });
+    }
+  }
+  componentDidMount() {
+    this.fetchBlogPosts();
+  }
   render() {
     return (
       <Row>
-        {posts.map((post) => (
-          <Col md={4} style={{ marginBottom: 50 }}>
-            <BlogItem key={post.title} {...post} />
-          </Col>
+        {this.state.blogPosts.map((post) => (
+          <>
+            {console.log(post)}
+            <Col md={4} style={{ marginBottom: 50 }}>
+              <BlogItem key={post.title} {...post} />
+            </Col>
+          </>
         ))}
       </Row>
     );
